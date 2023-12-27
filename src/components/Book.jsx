06f13c0239f250601f29d15fc9a661ec739e20d0
philmarbook <philmar@lilo.org>
2023-12-27 01:43:34 +0100

    feat: caption & scss in book component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,18 +1,27 @@
 import '../assets/scss/components/Book.scss';
 
-function Book({ title, imageSrc, price, url, caption}) {
+function Book({ title, imageSrc, price, url,intro00 ,intro01, caption00a, caption00b, caption01a, caption01b, caption02a, caption02b }) {
     return (
         <div className="book">
             <div>
                 <h1 className="book-title">{title}</h1>
                 <img className="book-image" src={imageSrc} alt={title} />
                 <p className="book-price">{price}</p>
-                <a href={url} target="_blank" rel="noopener noreferrer">
-                    <button className="book-button">Acheter</button>
-                </a>
+                <div className='book-button'>
+                    <a href={url} target="_blank" rel="noopener noreferrer">
+                        <button className="book-button-text">Acheter</button>
+                    </a>
+                </div>
             </div>
             <div className="book-caption">
-                {caption}
+                <p id="book-caption-part0a" className="book-caption-text">{intro00}</p>
+                <p id="book-caption-part0b" className="book-caption-text">{intro01}</p>
+                <p id="book-caption-part1a" className="book-caption-text">{caption00a}</p>
+                <p id="book-caption-part1a" className="book-caption-text">{caption00b}</p>
+                <p id="book-caption-part1b" className="book-caption-text">{caption01a}</p>
+                <p id="book-caption-part1b" className="book-caption-text">{caption01b}</p>
+                <p id="book-caption-part1c" className="book-caption-text">{caption02a}</p>
+                <p id="book-caption-part1c" className="book-caption-text">{caption02b}</p>
             </div>
         </div>
     )
