6c9daaba889e491c7a08c32834bd1ecd94f427b0
philmarbook <philmar@lilo.org>
2023-12-27 01:44:45 +0100

    feat: home component

--- src/pages/tabs/Home.jsx
+++ src/pages/tabs/Home.jsx
@@ -1,6 +1,18 @@
+import '../../assets/scss/pages/tabs/Home.scss';
+
 function Home() {
     return(
-        <p>l'accueil</p>
+        <div className='home'>
+            <div className='home-header'>
+                <ul>
+                    <li>Philmar - Auteur</li>
+                    <li>2 livres en auto-édition</li>
+                </ul>
+            </div>
+            <div className="home-caption">
+                <p>Découvrez l'auteur-artiste,<br />ses livres en vente,<br />et son actualité.</p>
+            </div>
+        </div>
     )
 }
 
