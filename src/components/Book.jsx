95d391e50d17fb675529cdfb39d7baa342f1b1de
philmarbook <philmar@lilo.org>
2024-01-02 00:03:40 +0100

    feat: book component caption

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,12 +1,13 @@
 import '../assets/scss/components/Book.scss';
 
-function Book({ title, imageSrc, price, url,intro00 ,intro01, caption00a, caption00b, caption01a, caption01b, caption02a, caption02b }) {
+function Book({ title, imageSrc, bookPrice, epubPrice, url,intro00 ,intro01, caption00a, caption00b, caption01a, caption01b, caption02a, caption02b, bulletpoint0, bulletpoint1, bulletpoint2, bulletpoint3, bulletpoint4, bulletpoint5 }) {
     return (
         <div className="book">
             <div>
                 <h1 className="book-title">{title}</h1>
                 <img className="book-image" src={imageSrc} alt={title} />
-                <p className="book-price">{price}</p>
+                <p className="book-price">{bookPrice}</p>
+                <p className="book-price">{epubPrice}</p>
                 <div className='book-button'>
                     <a href={url} target="_blank" rel="noopener noreferrer">
                         <button className="book-button-text">Acheter</button>
@@ -14,14 +15,28 @@ function Book({ title, imageSrc, price, url,intro00 ,intro01, caption00a, captio
                 </div>
             </div>
             <div className="book-caption">
-                <p id="book-caption-part0a" className="book-caption-text">{intro00}</p>
-                <p id="book-caption-part0b" className="book-caption-text">{intro01}</p>
-                <p id="book-caption-part1aa" className="book-caption-text">{caption00a}</p>
-                <p id="book-caption-part1ab" className="book-caption-text">{caption00b}</p>
-                <p id="book-caption-part1ba" className="book-caption-text">{caption01a}</p>
-                <p id="book-caption-part1bb" className="book-caption-text">{caption01b}</p>
-                <p id="book-caption-part1ca" className="book-caption-text">{caption02a}</p>
-                <p id="book-caption-part1cb" className="book-caption-text">{caption02b}</p>
+                <div className='book-caption-top'>
+                    <p id="book-caption-part0a" className="book-caption-top-text">{intro00}</p>
+                    <p id="book-caption-part0b" className="book-caption-top-text">{intro01}</p>
+                </div>
+                <div className='book-caption-bottom'>
+                    <div className='book-caption-bottom-titles'>
+                        <p id="book-caption-part1aa" className="book-caption-bottom-titles-text">{caption00a}</p>
+                        <p id="book-caption-part1ab" className="book-caption-bottom-titles-text">{caption00b}</p>
+                        <p id="book-caption-part1ba" className="book-caption-bottom-titles-text">{caption01a}</p>
+                        <p id="book-caption-part1bb" className="book-caption-bottom-titles-text">{caption01b}</p>
+                        <p id="book-caption-part1ca" className="book-caption-bottom-titles-text">{caption02a}</p>
+                        <p id="book-caption-part1cb" className="book-caption-bottom-titles-text">{caption02b}</p>
+                    </div>
+                    <div className='book-caption-bottom-bulletpoints'>
+                        <p id="book-caption-bulletpoint0" className="book-caption-bottom-bulletpoints-text">{bulletpoint0}</p>
+                        <p id="book-caption-bulletpoint1" className="book-caption-bottom-bulletpoints-text">{bulletpoint1}</p>
+                        <p id="book-caption-bulletpoint2" className="book-caption-bottom-bulletpoints-text">{bulletpoint2}</p>
+                        <p id="book-caption-bulletpoint3" className="book-caption-bottom-bulletpoints-text">{bulletpoint3}</p>
+                        <p id="book-caption-bulletpoint4" className="book-caption-bottom-bulletpoints-text">{bulletpoint4}</p>
+                        <p id="book-caption-bulletpoint5" className="book-caption-bottom-bulletpoints-text">{bulletpoint5}</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/pages/tabs/Books.jsx
+++ src/pages/tabs/Books.jsx
@@ -10,7 +10,8 @@ function Books() {
                 <Book
                 title="Les humains adorent les histoires ! 📖"
                 imageSrc={princesseDataImage}
-                price="15.00€"
+                bookPrice="Broché : 15.00€"
+                epubPrice="Digital : 6.00€"
                 url="https://www.pokepedia.fr/Pikachu"
                 intro00="Êtes vous prêts à voyager dans les contrées inconnues, les univers étranges et l'humour insolite d'un ouvrage déroulant avec moult couleurs dialogues, descriptions et réflexions?"
                 intro01="Si vous êtes intrigués, vous serez conquis, envoûtés par Les Humains adorent les Histoires."
@@ -19,22 +20,35 @@ function Books() {
                 caption01a="✍️ Style et Expression : "
                 caption01b="L'écriture est engageante et réfléchie, mêlant prose descriptive et analyse introspective pour captiver le lecteur dans le récit et stimuler la réflexion. L'auteur utilise une variété de techniques narratives, avec un style d'écriture à la fois accessible et profond."
                 caption02a="📚 Diversité des Sujets : "
-                caption02b="Le recueil couvre une large gamme d'émotions et d'expériences, depuis l'identité personnelle et collective aux analyses de la culture et de la société, en passant par les mythes et légendes, soulignant le rôle des histoires dans la vie humaine."/>
+                caption02b="Le recueil couvre une large gamme d'émotions et d'expériences, depuis l'identité personnelle et collective aux analyses de la culture et de la société, en passant par les mythes et légendes, soulignant le rôle des histoires dans la vie humaine."
+                bulletpoint0="📌 Reliure : Dos Carré Collé"
+                bulletpoint1="📌 Nombre de pages total : 84"
+                bulletpoint2="📌 Impression intérieur : Noir et blanc"
+                bulletpoint3="📌 Format : 16x24"
+                bulletpoint4="📌 Papier Intérieur : Bouffant 90g blanc"
+                bulletpoint5="📌 Pelliculage Couverture : Brillant"/>
             </div>
             <div>
                 <Book
                 title="Mélomorphoses 💫"
                 imageSrc={leveToiEtCodeImage}
-                price="15.00€"
+                bookPrice="Broché : 15.00€"
+                epubPrice="Digital : 6.00€"
                 url="https://www.pokepedia.fr/Mewtwo"
                 intro00="Êtes vous prêts à plonger dans les sentiments, les ferveurs, et les rêveries d'un ouvrage qui, tour à tour cisèle les mots ou les impulse?"
                 intro01="Si ça vous interpelle, Mélomorphoses vous touchera profondément."
                 caption00a="📝 Thématique Générale : "
-                caption00b="'Mélomorphoses' est un recueil de poèmes et de chansons qui traite de thèmes introspectifs et universels. Il explore la profondeur des sentiments humains, la beauté de la création artistique, et la relation complexe entre l'homme et le monde qui l'entoure."
+                caption00b="'Mélomorphoses' est un recueil de poèmes et de chansons qui traite de thèmes introspectifs et universels. Il explore la profondeur des sentiments humains, la beauté de la création artistique, et la relation complexe entre l'humain et le monde qui l'entoure."
                 caption01a="✍️ Style et Expression : "
                 caption01b="Les textes sont écrits avec une touche lyrique et métaphorique, invitant à une réflexion sur des sujets tels que l'existence, l'amour, et la quête de sens. L'utilisation des images poétiques enrichit l'expression des émotions et des idées."
                 caption02a="📚 Diversité des Sujets : "
-                caption02b="Le recueil semble couvrir un large éventail de sujets, allant de l'exploration personnelle à la contemplation de la nature et de l'univers, en passant par la célébration de l'art et de la créativité."/>
+                caption02b="Le recueil semble couvrir un large éventail de sujets, allant de l'exploration personnelle à la contemplation de la nature et de l'univers, en passant par la célébration de l'art et de la créativité."
+                bulletpoint0="📌 Reliure : Dos Carré Collé"
+                bulletpoint1="📌 Nombre de pages total : 90"
+                bulletpoint2="📌 Impression intérieur : Noir et blanc"
+                bulletpoint3="📌 Format : 16x24"
+                bulletpoint4="📌 Papier Intérieur : Bouffant 90g blanc"
+                bulletpoint5="📌 Pelliculage Couverture : Brillant"/>
             </div>
         </>
     )
