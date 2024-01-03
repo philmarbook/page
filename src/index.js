7f43301d74fd4638408feb2886c1a6129b7f86d8
philmarbook <philmar@lilo.org>
2024-01-03 02:17:11 +0100

    feat: index.scss

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './router/App';
+import './assets/scss/index.scss'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
