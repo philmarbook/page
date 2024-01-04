bcc6b08a9803aca91e164324406bad1ba78ca463
philmarbook <philmar@lilo.org>
2024-01-04 13:18:21 +0100

    fix: links offest

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -14,10 +14,10 @@ function Main() {
             <header className="header">
                 <nav>
                     <ul>
-                        <li className="tab"><Link className="nav-link" to="home" smooth={true} duration={500}>Accueil</Link></li>
-                        <li className="tab"><Link className="nav-link" to="books" smooth={true} duration={500}>Livres</Link></li>
-                        <li className="tab"><Link className="nav-link" to="bio" smooth={true} duration={500}>Bio</Link></li>
-                        <li className="tab"><Link className="nav-link" to="contact" smooth={true} duration={500}>Contact</Link></li>
+                        <li className="tab"><Link className="nav-link" to="home" smooth={true} duration={500} offset={-50}>Accueil</Link></li>
+                        <li className="tab"><Link className="nav-link" to="books" smooth={true} duration={500} offset={-50}>Livres</Link></li>
+                        <li className="tab"><Link className="nav-link" to="bio" smooth={true} duration={500} offset={-80}>Bio</Link></li>
+                        <li className="tab"><Link className="nav-link" to="contact" smooth={true} duration={500} offset={-50}>Contact</Link></li>
                     </ul>
                 </nav>
             </header>
