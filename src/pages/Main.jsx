ce3a54a596f76970aa4f163017f45b210e6ab342
philmarbook <philmar@lilo.org>
2024-01-26 18:32:30 +0100

    feat: events component

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './tabs/Home'; 
 import Books from './tabs/Books';
+import Events from './tabs/Events';
 import Bio from './tabs/Bio'; 
 import Contact from './tabs/Contact'; 
 import '../assets/scss/pages/Main.scss';
@@ -17,6 +18,7 @@ function Main() {
                         <li className="tab"><Link className="nav-link" to="home" smooth={true} duration={500} offset={-50}>Accueil</Link></li>
                         <li className="tab"><Link className="nav-link" to="books" smooth={true} duration={500} offset={-50}>Livres</Link></li>
                         <li className="tab"><Link className="nav-link" to="bio" smooth={true} duration={500} offset={-220}>Bio</Link></li>
+                        <li className="tab"><Link className="nav-link" to="events" smooth={true} duration={500} offset={-220}>Évènements</Link></li>
                         <li className="tab"><Link className="nav-link" to="contact" smooth={true} duration={500} offset={-50}>Contact</Link></li>
                     </ul>
                 </nav>
@@ -30,7 +32,12 @@ function Main() {
             <div id="bio"><Bio
             srcImage={philmarImage}
             /></div>
+            <div id='events-title'>
+                <h2>Évènements</h2>
+            </div>
+            <div id="events"><Events /> </div>
             <div id="contact"><Contact /></div>
+            
 
             <footer className="footer">
                 <p>© Copyright PhilmarZic All Rights Reserved</p>
--- src/pages/tabs/Events.jsx
+++ src/pages/tabs/Events.jsx
@@ -1,7 +1,34 @@
+import '../../assets/scss/pages/tabs/Events.scss';
+
+
 function Events() {
     return(
-        <p></p>
+        <div className='events'>
+            <div className="events-container">
+                <div className='events-block'>
+                    <h1 className='events-block-title'>Rencontre à la Librairie Livres en Tête</h1>
+                    <h2 className='events-block-date'>Jeudi 22 février 18:00 à 19:30</h2>
+                    <p className='events-block-location'>82 Rue du Mont Joly, 74700 SALLANCHES, France</p>
+                    <p className='events-block-number'>04 50 93 92 40</p>
+                    <div>
+                        <p className='events-block-program'></p>
+                        <p className='events-block-program'></p>
+                        <p className='events-block-program'></p>
+                    </div>
+                </div>
+                <div className='events-block'>
+                    <h1 className='events-block-title'>Rencontre à la Librairie des Bauges</h1>
+                    <h2 className='events-block-date'>Samedi 16 mars 17:00 à 19:00</h2>
+                    <p className='events-block-location'>104 rue de la République, 73200 ALBERTVILLE, France</p>
+                    <p className='events-block-number'>04 79 32 00 91</p>
+                    <div>
+                        <p className='events-block-program'></p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default Events
+export default Events;
+
